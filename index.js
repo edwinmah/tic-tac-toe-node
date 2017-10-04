6c4292ce8c5dae629e55c380146ce20a47ba5273
Edwin Mah <edwinmah@users.noreply.github.com>
2017-10-04 14:31:04 -0400

    Declare winning combos, board, and players

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const prompt = require('prompt');
 
 (function() {
+  const WINNING_COMBOS = [
+    // Horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Diagonal
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
 
+  let board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      human, computer;
 })();
