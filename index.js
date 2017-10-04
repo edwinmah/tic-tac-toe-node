676454ef6a0627fe892cbff4710ed736cac9acf6
Edwin Mah <edwinmah@users.noreply.github.com>
2017-10-04 14:32:29 -0400

    Display the playing board

--- index.js
+++ index.js
@@ -40,4 +40,19 @@ const prompt = require('prompt');
       });
     });
   }
+
+  function displayBoard() {
+    return new Promise((resolve, reject) => {
+      console.log(
+        `\n        +---+---+---+
+        | ${board[0]} | ${board[1]} | ${board[2]} |
+        -------------
+        | ${board[3]} | ${board[4]} | ${board[5]} |
+        -------------
+        | ${board[6]} | ${board[7]} | ${board[8]} |
+        +---+---+---+\n`
+      );
+      resolve(human);
+    });
+  }
 })();
